4e30f8a7e281000a7549fa6c8d24327a464a55eb
Bar Angel <bar.ang16@gmail.com>
2024-12-14 12:55:27 +0200

    board.go: SetStartingPos()

--- chess/board.go
+++ chess/board.go
@@ -19,3 +19,50 @@ func NewChessBoard() *Board {
         pieces: pieces,
     }
 }
+
+func (b *Board) setKingsInStartingPos() {
+    b.pieces[0][4] = NewPiece(PieceKing, PlayerBlack)
+    b.pieces[BoardSize-1][3] = NewPiece(PieceKing, PlayerWhite)
+}
+
+func (b *Board) setQueensInStartingPos() {
+    b.pieces[0][3] = NewPiece(PieceQueen, PlayerBlack)
+    b.pieces[BoardSize-1][4] = NewPiece(PieceQueen, PlayerWhite)
+}
+
+func (b *Board) setBishopsInStartingPos() {
+    b.pieces[0][2] = NewPiece(PieceBishop, PlayerBlack)
+    b.pieces[0][5] = NewPiece(PieceBishop, PlayerBlack)
+    b.pieces[BoardSize-1][2] = NewPiece(PieceBishop, PlayerWhite)
+    b.pieces[BoardSize-1][5] = NewPiece(PieceBishop, PlayerWhite)
+}
+
+func (b *Board) setKnightsInStartingPos() {
+    b.pieces[0][1] = NewPiece(PieceKnight, PlayerBlack)
+    b.pieces[0][6] = NewPiece(PieceKnight, PlayerBlack)
+    b.pieces[BoardSize-1][1] = NewPiece(PieceKnight, PlayerWhite)
+    b.pieces[BoardSize-1][6] = NewPiece(PieceKnight, PlayerWhite)
+}
+
+func (b *Board) setRooksInStartingPos() {
+    b.pieces[0][0] = NewPiece(PieceRook, PlayerBlack)
+    b.pieces[0][7] = NewPiece(PieceRook, PlayerBlack)
+    b.pieces[BoardSize-1][0] = NewPiece(PieceRook, PlayerWhite)
+    b.pieces[BoardSize-1][7] = NewPiece(PieceRook, PlayerWhite)
+}
+
+func (b *Board) setPawnsInStartingPos() {
+    for i := 0; i < 8; i++ {
+        b.pieces[1][i] = NewPiece(PiecePawn, PlayerBlack)
+        b.pieces[6][i] = NewPiece(PiecePawn, PlayerWhite)
+    }
+}
+
+func (b *Board) SetStartingPos() {
+    b.setKingsInStartingPos()
+    b.setQueensInStartingPos()
+    b.setBishopsInStartingPos()
+    b.setKnightsInStartingPos()
+    b.setRooksInStartingPos()
+    b.setPawnsInStartingPos()
+}
