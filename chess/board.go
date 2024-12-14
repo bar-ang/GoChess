9ae32f18a07130221f241844a0b31a6ddb27300a
Bar Angel <bar.ang16@gmail.com>
2024-12-14 12:24:40 +0200

    introducing: board.go

---
+++ chess/board.go
@@ -0,0 +1,21 @@
+package chess
+
+const BoardSize = 8
+
+type Board struct {
+    pieces [][]Piece
+}
+
+func (b *Board) NewChessBoard() *Board {
+    pieces := make([][]Piece, BoardSize)
+    for i, _ := range pieces {
+        pieces[i] = make([]Piece, BoardSize)
+        for j, _ := range pieces[i] {
+            pieces[i][j] = NoPiece()
+        }
+    }
+
+    return &Board{
+        pieces: pieces,
+    }
+}
