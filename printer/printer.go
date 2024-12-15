187ffa2aa5a834d87d4d50255422a88b8e57b5a1
Bar Angel <bar.ang16@gmail.com>
2024-12-15 13:07:31 +0200

    introducing: printer.go

---
+++ printer/printer.go
@@ -0,0 +1,35 @@
+package printer
+
+import (
+    "fmt"
+    "goChess/chess"
+)
+
+func ChessPieceToString(piece chess.Piece) string {
+    switch t := piece.Type(); t {
+    case chess.PieceKing:
+        return "K"
+    case chess.PieceQueen:
+        return "Q"
+    case chess.PieceKnight:
+        return "N"
+    case chess.PieceRook:
+        return "R"
+    case chess.PieceBishop:
+        return "B"
+    case chess.PiecePawn:
+        return "P"
+    default:
+        return " "
+    }
+}
+
+func PrintChessBoard(board *chess.Board) {
+    for i := 0; i < chess.BoardSize; i++ {
+        for j := 0; j < chess.BoardSize; j++ {
+            p := board.GetPiece(i, j)
+            fmt.Printf("%v", ChessPieceToString(p))
+        }
+        fmt.Printf("\n")
+    }
+}
