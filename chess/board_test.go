c11ba9475082596708825fd9488381d702e55fc2
Bar Angel <bar.ang16@gmail.com>
2024-12-20 20:24:59 +0200

    board_test.go: test possible moves and threats for Rook, Bishop, Queen

--- chess/board_test.go
+++ chess/board_test.go
@@ -37,10 +37,136 @@ func TestSelectBasic(t *testing.T) {
 }
 
 func TestPieceSelectionOnEmptyBoard(t *testing.T) {
-    t.Run("SelectRook", func(t *testing.T) {
+    t.Run("SelectRookInCenter", func(t *testing.T) {
         board := NewChessBoard()
         board.SetPiece(3, 2, NewPiece(PieceRook, PlayerWhite))
-        _, err := board.SelectPiece(3, 2)
+        sel, err := board.SelectPiece(3, 2)
         require.NoError(t, err)
+        require.NotEmpty(t, sel.possibleMoves)
+        require.Empty(t, sel.threatenPieces)
+
+        require.Len(t, sel.possibleMoves, 14)
+
+        require.Contains(t, sel.possibleMoves, sqr(3, 0))
+        require.Contains(t, sel.possibleMoves, sqr(3, 1))
+        require.Contains(t, sel.possibleMoves, sqr(3, 3))
+        require.Contains(t, sel.possibleMoves, sqr(3, 4))
+        require.Contains(t, sel.possibleMoves, sqr(3, 5))
+        require.Contains(t, sel.possibleMoves, sqr(3, 6))
+        require.Contains(t, sel.possibleMoves, sqr(3, 7))
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 2))
+        require.Contains(t, sel.possibleMoves, sqr(1, 2))
+        require.Contains(t, sel.possibleMoves, sqr(2, 2))
+        require.Contains(t, sel.possibleMoves, sqr(4, 2))
+        require.Contains(t, sel.possibleMoves, sqr(5, 2))
+        require.Contains(t, sel.possibleMoves, sqr(6, 2))
+        require.Contains(t, sel.possibleMoves, sqr(7, 2))
+    })
+    t.Run("SelectRookInCorner", func(t *testing.T) {
+        board := NewChessBoard()
+        board.SetPiece(7, 7, NewPiece(PieceRook, PlayerWhite))
+        sel, err := board.SelectPiece(7, 7)
+        require.NoError(t, err)
+        require.NotEmpty(t, sel.possibleMoves)
+        require.Empty(t, sel.threatenPieces)
+
+        require.Len(t, sel.possibleMoves, 14)
+
+        require.Contains(t, sel.possibleMoves, sqr(7, 0))
+        require.Contains(t, sel.possibleMoves, sqr(7, 1))
+        require.Contains(t, sel.possibleMoves, sqr(7, 2))
+        require.Contains(t, sel.possibleMoves, sqr(7, 3))
+        require.Contains(t, sel.possibleMoves, sqr(7, 4))
+        require.Contains(t, sel.possibleMoves, sqr(7, 5))
+        require.Contains(t, sel.possibleMoves, sqr(7, 6))
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 7))
+        require.Contains(t, sel.possibleMoves, sqr(1, 7))
+        require.Contains(t, sel.possibleMoves, sqr(3, 7))
+        require.Contains(t, sel.possibleMoves, sqr(2, 7))
+        require.Contains(t, sel.possibleMoves, sqr(4, 7))
+        require.Contains(t, sel.possibleMoves, sqr(5, 7))
+        require.Contains(t, sel.possibleMoves, sqr(6, 7))
+    })
+    t.Run("SelectBishopInCenter", func(t *testing.T) {
+        board := NewChessBoard()
+        board.SetPiece(2, 3, NewPiece(PieceBishop, PlayerBlack))
+        sel, err := board.SelectPiece(2, 3)
+        require.NoError(t, err)
+        require.NotEmpty(t, sel.possibleMoves)
+        require.Empty(t, sel.threatenPieces)
+
+        require.Len(t, sel.possibleMoves, 11)
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 1))
+        require.Contains(t, sel.possibleMoves, sqr(1, 2))
+        require.Contains(t, sel.possibleMoves, sqr(3, 4))
+        require.Contains(t, sel.possibleMoves, sqr(4, 5))
+        require.Contains(t, sel.possibleMoves, sqr(5, 6))
+        require.Contains(t, sel.possibleMoves, sqr(6, 7))
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 5))
+        require.Contains(t, sel.possibleMoves, sqr(1, 4))
+        require.Contains(t, sel.possibleMoves, sqr(3, 2))
+        require.Contains(t, sel.possibleMoves, sqr(4, 1))
+        require.Contains(t, sel.possibleMoves, sqr(5, 0))
+    })
+    t.Run("SelectBishopInCorner", func(t *testing.T) {
+        board := NewChessBoard()
+        board.SetPiece(0, 7, NewPiece(PieceBishop, PlayerBlack))
+        sel, err := board.SelectPiece(0, 7)
+        require.NoError(t, err)
+        require.NotEmpty(t, sel.possibleMoves)
+        require.Empty(t, sel.threatenPieces)
+
+        require.Len(t, sel.possibleMoves, 7)
+
+        require.Contains(t, sel.possibleMoves, sqr(1, 6))
+        require.Contains(t, sel.possibleMoves, sqr(2, 5))
+        require.Contains(t, sel.possibleMoves, sqr(3, 4))
+        require.Contains(t, sel.possibleMoves, sqr(4, 3))
+        require.Contains(t, sel.possibleMoves, sqr(5, 2))
+        require.Contains(t, sel.possibleMoves, sqr(6, 1))
+        require.Contains(t, sel.possibleMoves, sqr(7, 0))
+    })
+    t.Run("SelectQueenInCenter", func(t *testing.T) {
+        board := NewChessBoard()
+        board.SetPiece(2, 3, NewPiece(PieceQueen, PlayerWhite))
+        sel, err := board.SelectPiece(2, 3)
+        require.NoError(t, err)
+        require.NotEmpty(t, sel.possibleMoves)
+        require.Empty(t, sel.threatenPieces)
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 1))
+        require.Contains(t, sel.possibleMoves, sqr(1, 2))
+        require.Contains(t, sel.possibleMoves, sqr(3, 4))
+        require.Contains(t, sel.possibleMoves, sqr(4, 5))
+        require.Contains(t, sel.possibleMoves, sqr(5, 6))
+        require.Contains(t, sel.possibleMoves, sqr(6, 7))
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 5))
+        require.Contains(t, sel.possibleMoves, sqr(1, 4))
+        require.Contains(t, sel.possibleMoves, sqr(3, 2))
+        require.Contains(t, sel.possibleMoves, sqr(4, 1))
+        require.Contains(t, sel.possibleMoves, sqr(5, 0))
+
+        require.Contains(t, sel.possibleMoves, sqr(2, 0))
+        require.Contains(t, sel.possibleMoves, sqr(2, 1))
+        require.Contains(t, sel.possibleMoves, sqr(2, 2))
+        require.Contains(t, sel.possibleMoves, sqr(2, 4))
+        require.Contains(t, sel.possibleMoves, sqr(2, 5))
+        require.Contains(t, sel.possibleMoves, sqr(2, 6))
+        require.Contains(t, sel.possibleMoves, sqr(2, 7))
+
+        require.Contains(t, sel.possibleMoves, sqr(0, 3))
+        require.Contains(t, sel.possibleMoves, sqr(1, 3))
+        require.Contains(t, sel.possibleMoves, sqr(3, 3))
+        require.Contains(t, sel.possibleMoves, sqr(4, 3))
+        require.Contains(t, sel.possibleMoves, sqr(5, 3))
+        require.Contains(t, sel.possibleMoves, sqr(6, 3))
+        require.Contains(t, sel.possibleMoves, sqr(7, 3))
+
+        require.Len(t, sel.possibleMoves, 7*2 + 11)
     })
 }
